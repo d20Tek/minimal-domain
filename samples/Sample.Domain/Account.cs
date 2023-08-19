fad0c20ce1514af6f52406850252a0159240d945
Pedro Silva <pedrosi@microsoft.com>
2023-08-19 12:50:26 -0700

    Added a sample domain project to show how to use the Patterns.Domain package.

---
+++ samples/Sample.Domain/Account.cs
@@ -0,0 +1,34 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+using D20Tek.Patterns.Domain;
+
+namespace Sample.Domain;
+
+public sealed class Account : Entity<AccountId>
+{
+    public string GivenName { get; set; } = default!;
+
+    public string FamilyName { get; set; } = default!;
+
+    public string Email { get; set; } = default!;
+
+    private Account(AccountId id,
+        string givenName,
+        string familyName,
+        string email)
+        : base(id)
+    {
+        GivenName = givenName;
+        FamilyName = familyName;
+        Email = email;
+    }
+
+    public static Account Create(
+        string givenName,
+        string familyName,
+        string email)
+    {
+        return new(AccountId.CreateUnique(), givenName, familyName, email);
+    }
+}
