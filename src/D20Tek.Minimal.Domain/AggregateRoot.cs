d45bae17fb0bd81191eaad66e32b39ad27f08ba6
DarthPedro <darthpedro@gmail.com>
2023-08-28 15:50:34 -0700

    Added AggregateRoot class derived from Entity. (#2)
    Added IDomainEvent and IDomainEventHandler for use in pub-sub event processing.
    Added unit tests.

---
+++ src/D20Tek.Minimal.Domain/AggregateRoot.cs
@@ -0,0 +1,24 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+using D20Tek.Minimal.Domain.Abstractions;
+
+namespace D20Tek.Minimal.Domain;
+
+public abstract class AggregateRoot<TId> : Entity<TId>
+    where TId : notnull
+{
+    private readonly List<IDomainEvent> _domainEvents = new();
+
+    public AggregateRoot(TId id)
+        : base(id)
+    {
+    }
+
+    public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+    public void ClearDomainEvents() => _domainEvents.Clear();
+
+    protected void AddDomainEvent(IDomainEvent domainEvent) =>
+        _domainEvents.Add(domainEvent);
+}
